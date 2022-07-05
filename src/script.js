c4edfda0eb8de3402bd3a686f23bc074a88c762c
sengel4 <siengel1121@gmail.com>
2022-07-05 12:13:15 -0400

    Troubleshooting Icon Issue

--- src/script.js
+++ src/script.js
@@ -104,7 +104,6 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
