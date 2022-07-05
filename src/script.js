70691bc9a4e8c2928e636255ebb0c4acd5cf6302
sengel4 <siengel1121@gmail.com>
2022-07-05 12:07:55 -0400

    Security

--- src/script.js
+++ src/script.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/${
+          src="https://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
